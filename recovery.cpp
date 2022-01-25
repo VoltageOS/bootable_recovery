5212995a9bdc5b1d48b14487d7556aeda4b1932d
Kanishk <prometheusXderpgang@gmail.com>
2022-01-25 10:24:03 +0530

    Test

--- recovery.cpp
+++ recovery.cpp
@@ -826,7 +826,7 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
 
   std::vector<std::string> title_lines = {
     "Version: " + android::base::GetProperty("ro.voltage.version", "(unknown)") +
-        "-" + ver_date,
+        + ver_date,
   };
   if (android::base::GetBoolProperty("ro.build.ab_update", false)) {
     std::string slot = android::base::GetProperty("ro.boot.slot_suffix", "");
